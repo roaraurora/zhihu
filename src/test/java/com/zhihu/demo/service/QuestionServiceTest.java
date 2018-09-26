4669264821633d9e3daee1d72303a09b33d9ce4b
deng <chao.deng@definesys.com>
2018-09-26 17:08:23 +0800

    [重构] 若干功能实现的重构

--- src/test/java/com/zhihu/demo/service/QuestionServiceTest.java
+++ src/test/java/com/zhihu/demo/service/QuestionServiceTest.java
@@ -16,30 +16,8 @@ import java.util.Set;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class QuestionServiceTest {
-    @Autowired
-    QuestionService questionService;
     @Test
     public void getQuestionListByUID() {
 
-    }
-
-    @Test
-    public void getQuestionByQids() {
-        Set<String> s = new HashSet<String>();
-        s.add("1060");
-        s.add("1061");
-        s.add("1062");
-        List<String> list1 = new ArrayList<String>(s);
-        List<Integer> list2 = null;
-        list2 = parseIntegersList(list1);
-        int i = questionService.getQuestionByQids(list2).getData().size();
-        Assert.assertEquals(2,i);
-    }
-    private List<Integer> parseIntegersList(List<String> StringList) {
-        List<Integer> IntegerList = new ArrayList<Integer>();
-        for (String x : StringList) {
-            Integer z = Integer.parseInt(x);
-            IntegerList.add(z);
-        }      	return IntegerList;
     }
 }
